74c2b0074e30c7f9c08ce5f27893a056c8f2c4fd
John Schmidley <tlg8n@outlook.com>
2019-08-21 17:01:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,9 +10,7 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
-def introduction_with_language_optional(name, language="Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+
 
 
 
